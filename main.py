3d683ae3aa15b1f36b78094537056c176819fe62
unknown <talk2archan@gmail.com>
2021-09-10 22:51:01 -0400

    checks

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def sample_eig_default(data_matrix, s, scale=False, rankcheck=0):
 trials = 20
 similarity_measure = "default" #"tps", "sigmoid" for kong, "default" for binary and random_sparse
 search_rank = [0,1,2,3,-4,-3,-2,-1]
-dataset_name = "facebook" #"binary", "kong", "asymmetric", "facebook", "arxiv", "block"
+dataset_name = "block" #"binary", "kong", "asymmetric", "facebook", "arxiv", "block"
 if dataset_name == "arxiv":
     max_samples = 5000
 else:
