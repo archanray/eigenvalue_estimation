61c7bbebd37cd690d5f2ad7135530349e8edeb56
unknown <talk2archan@gmail.com>
2021-11-26 21:37:34 -0500

    new eigval plot

--- test_create_LB.py
+++ test_create_LB.py
@@ -1,18 +1,55 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+from random import sample
 
 n = 2500
 eps = 0.1
+
 A = np.ones((n,n))
-num_blocks = int(1/(eps**4))
-sample_block_sizes = int((eps**2)*n)
-list_of_indices = list(range(n))
+num_blocks = round(1/(eps**2))
+sample_block_sizes = round((eps**2)*n)
 
-samples = []
+R = np.zeros_like(A)
+Z = [-1, 1]
+
+sample_block = np.ones((sample_block_sizes, sample_block_sizes))
+
+block_start_row = []
+block_end_row = []
+block_start_col = []
+block_end_col = []
+start_row = 0
+start_col = 0
 for i in range(num_blocks):
-	print(len(list_of_indices), sample_block_sizes, num_blocks)
-	sampled_indices = random.sample(list_of_indices, k=sample_block_sizes)
-	list_of_indices = list(set(list_of_indices) - set(sampled_indices))
-	samples.append(sampled_indices)
+	block_start_row.append(start_row)
+	block_start_col.append(start_col)
+	start_row += sample_block_sizes
+	start_col += sample_block_sizes
+	block_end_row.append(start_row)
+	block_end_col.append(start_col)
 
-print(len(samples))
+
+row_id = 0
+for i in range(num_blocks):
+	col_id = 0
+
+	for j in range(num_blocks):
+		q = int(np.unique(R[block_start_row[i]:block_end_row[i], block_start_col[j]:block_end_col[j]])[-1])
+
+		if q == 0:
+			flag = sample(Z,1)[-1]
+			R[block_start_row[i]:block_end_row[i], block_start_col[j]:block_end_col[j]] \
+						= int(flag)*sample_block
+
+			R[block_start_row[j]:block_end_row[j], block_start_col[i]:block_end_col[i]] \
+						= flag*sample_block
+
+
+A = A+R
+
+eigvals, eigvecs = np.linalg.eig(A)
+plt.plot(range(n), np.diag(eigvecs))
+plt.xlabel("indices")
+plt.ylabel("eigenvalues")
+plt.show()
