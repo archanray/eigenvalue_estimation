0c2c15f355da4857a073200d2dc209f849a3ab3a
Archan Ray <ray@swarm.cs.umass.edu>
2022-04-21 11:56:36 -0400

    swarm push

--- run_swarm.sh
+++ run_swarm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sbatch -J sweep_eps -o sweep_eps.out -e sweep_eps.err --nodes 6 --cpus-per-task 12 -p longq --mem 32000 run_swarm.sh
 
-python test_for_eps_power.py 
+python compare_unified.py 
