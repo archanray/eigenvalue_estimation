90b9d7db1817be623509814f40d6335b4a419585
unknown <talk2archan@gmail.com>
2022-04-25 13:43:26 -0400

    tridiagonal matrix

--- src/get_dataset.py
+++ src/get_dataset.py
@@ -423,7 +423,7 @@ def get_data(name, eps=0.1, plot_mat=True, raise_eps=False):
         matrix for zeros in the main diagonal and 1s in the 1st off-diagonal
         """
         from scipy.sparse import diags
-        dataset_size = 5000
+        dataset_size = 1000
         diagonals = [list(np.zeros(dataset_size)), list(np.ones(dataset_size-1)), list(np.ones(dataset_size-1))]
         A = diags(diagonals, [0, -1, 1]).toarray()
 
