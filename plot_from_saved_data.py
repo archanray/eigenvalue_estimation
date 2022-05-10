e8b252daef9752b18dbd30d08cdeaa517883b43b
unknown <talk2archan@gmail.com>
2022-05-09 22:13:29 -0400

    fixed plots

---
+++ plot_from_saved_data.py
@@ -0,0 +1,16 @@
+import pickle
+from src.viz import plot_all_errors, plot_all_nnz
+
+dataset_name = "arxiv"
+name_adder = "uniform_nnz_sparse"
+
+with open("pickle_files/"+dataset_name+"_"+name_adder+".pkl", "rb") as f:
+    [tracked_errors, tracked_percentile1, tracked_percentile2, sampling_modes, dataset_name, dataset_size, \
+            search_rank, max_samples, min_samples, \
+            true_spectrum, name_adder] = pickle.load(f)
+
+# visualize the errors
+plot_all_errors(tracked_errors, tracked_percentile1, tracked_percentile2, \
+            sampling_modes, dataset_name, dataset_size, \
+            search_rank, max_samples, min_samples, \
+            true_spectrum, name_adder)
